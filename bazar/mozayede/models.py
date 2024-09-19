78a88ae858f77dc4b228512daf1a088d20d056ea
Alireza Shams <alerzs1023@gmail.com>
2024-09-19 11:42:14 +0330

    ggyhukli

--- bazar/mozayede/models.py
+++ bazar/mozayede/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Customer(models.Model):
+    user = models.OneToOneField(User)
+
+class Provider(models.Model):
+    user = models.OneToOneField(User)
+
+class Auction(models.Model):
+    start_price =models.IntegerField()
+    current_price =models.IntegerField(default=start_price ,blank=True,null=True)
+    mahsool = models.CharField(max_length=15)
+    provider = models.ForeignKey(Provider , on_delete=models.CASCADE ,related_name="prov")
+    last_custoer = models.ForeignKey(Customer , on_delete=models.CASCADE ,related_name="cust")
+    
+
