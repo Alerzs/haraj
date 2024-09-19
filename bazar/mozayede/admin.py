0b8d0e5e195be283a64330ba5c210a111635c84a
Fatememdn <mohammadianfateme86@gmail.com>
2024-09-19 12:16:08 +0330

    admin file

--- bazar/mozayede/admin.py
+++ bazar/mozayede/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Customer, Provider, Auction
 
-# Register your models here.
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Provider)
+class ProviderAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Auction)
+class AuctionAdmin(admin.ModelAdmin):
+    pass
+
